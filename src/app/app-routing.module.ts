d19178960001a2a16a392375b3b196af7dbbe79b
Davemester <gyuracz87@gmail.com>
2020-03-27 17:03:47 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TypographyComponent } from './components/typography/typography.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { ToggleButtonsComponent } from './components/toggle-buttons/toggle-buttons.component';
+import { MatIconsComponent } from './components/mat-icons/mat-icons.component';
+import { MatlistComponent } from './components/matlist/matlist.component';
+import { ExpansionPanelComponent } from './components/expansion-panel/expansion-panel.component';
+import { MatCardsComponent } from './components/mat-cards/mat-cards.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'typography', component: TypographyComponent},
+  {path: 'buttons', component: ButtonsComponent},
+  {path: 'button-toggles', component: ToggleButtonsComponent},
+  {path: 'mat-icons', component: MatIconsComponent},
+  {path: 'mat-list', component: MatlistComponent},
+  {path: 'mat-exp-panel', component: ExpansionPanelComponent},
+  {path: 'mat-cards', component: MatCardsComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from '../app/material/material.module';
+import { TypographyComponent } from './components/typography/typography.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { ToggleButtonsComponent } from './components/toggle-buttons/toggle-buttons.component';
+import { MatIconsComponent } from './components/mat-icons/mat-icons.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import {FormsModule} from '@angular/forms';
+import { MatlistComponent } from './components/matlist/matlist.component';
+import { ExpansionPanelComponent } from './components/expansion-panel/expansion-panel.component';
+import { MatCardsComponent } from './components/mat-cards/mat-cards.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypographyComponent,
+    ButtonsComponent,
+    ToggleButtonsComponent,
+    MatIconsComponent,
+    SidenavComponent,
+    MatlistComponent,
+    ExpansionPanelComponent,
+    MatCardsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
